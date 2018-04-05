3a25d035f987d712c96e87b3b3ea5d60a0e91b69
Maksym_Dryha <maksym_dryha@epam.com>
2018-04-05 15:49:22 +0300

    asdasd

--- task-03/js/table.js
+++ task-03/js/table.js
@@ -12,18 +12,37 @@ window.onload = function() {
   renderBtn.addEventListener('click', function(e) {
     config = getConfig();
     tableContent = multiplicationTable(...config);
-    console.log(tableContent);
-    renderTable();
+    renderTable(tableContent);
     e.preventDefault();
   });
 
-  function renderTable() {
+  function renderTable(tableContent) {
     clearTable();
+    var tableFragment = document.createDocumentFragment();
+    var   thead = tableFragment.appendChild(document.createElement('thead')),
+          tbody = tableFragment.appendChild(document.createElement('tbody'));  
+
+    for(let i = 0; i < tableContent.length; i++) {
+      let row = document.createElement('tr');
+      if(i === 0) {
+        thead.appendChild(row);
+      } else {
+        tbody.appendChild(row);
+      }
+      for(let j = 0; j < tableContent[i].length; j++) {
+        let elementToInser = i === 0 ? 'th': j === 0 ? 'th': 'td';
+        let element = document.createElement(elementToInser);
+        element.innerText = tableContent[i][j];
+        row.appendChild(element);
+      }
+    }
+    table.appendChild(tableFragment);
   }
 
   function clearTable() {
     var newTable = table.cloneNode(false);
     table.parentElement.replaceChild(newTable, table);
+    table = document.getElementsByClassName('table')[0];
   }
 
   function getConfig() {
