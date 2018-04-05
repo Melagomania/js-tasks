7393cccbb1f077471bd183262ac60a6ff7a3ebb3
Maksym_Dryha <maksym_dryha@epam.com>
2018-04-05 13:05:32 +0300

    refactor

--- task-02/task2.js
+++ task-02/task2.js
@@ -1,4 +1,3 @@
-//ES5
 function passToCallbackES5() {
   var args = [].slice.call(arguments, 0);
   var callback = args.pop();
@@ -8,7 +7,6 @@ function passToCallbackES5() {
   return callback.apply(null, args);
 }
 
-//ES6
 function passToCallbackES6 ()  {
   let args = Array.from(arguments);
   let callback = args.pop();
@@ -18,13 +16,10 @@ function passToCallbackES6 ()  {
   return callback(...args);
 } 
 
-
-//ES5
 console.log(
   passToCallbackES5(1, 2, 3, 5, (...args) => args.map(num => num * 2)),
   passToCallbackES5(4, 3, 2, 1, (...args) => args.reduce((cur, prev) => prev * cur))
 );
-//ES6
 console.log(
   passToCallbackES6(1, 2, 3, 5, (...args) => args.map(num => num * 2)),
   passToCallbackES6(4, 3, 2, 1, (...args) => args.reduce((cur, prev) => prev * cur))
